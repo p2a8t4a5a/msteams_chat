e225095c3aaed3a2f2f4121bce948cb53302c1f9
lexagr <lexa.grydnistiy123@gmail.com>
2020-05-27 19:04:18 +0300

    Structure validate function implementation

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -5,6 +5,19 @@ export namespace Utils {
         return obj.every(e => e != undefined && e != null);
     }
 
+    export function structIsValid(approx_struct: any, obj: any){
+        for(let key in approx_struct){
+            if(!obj[key] || typeof(approx_struct[key]) == "object" && !structIsValid(approx_struct[key], obj[key])) return false;
+            
+            if(typeof(approx_struct[key]) == "string" && approx_struct[key].split('/').filter((type: string) => {
+                type = type.trim();
+                return type.length > 0 && type == typeof(obj[key]);
+            }).length <= 0) return false; // type mismatch
+        }
+    
+        return true;
+    }
+
     export function createWindow(width:number, height: number, source: string, addition_options?: BrowserWindowConstructorOptions) : BrowserWindow {
         let win = new BrowserWindow(Object.assign({
             width,
